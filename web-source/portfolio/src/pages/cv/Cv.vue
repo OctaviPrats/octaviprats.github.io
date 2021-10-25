fd303166ebc3335e9e6a045d9521f6734e7601af
octaviprats <octavi.prats@est.fib.upc.edu>
2021-10-26 00:50:22 +0200

    hard pivot

---
+++ web-source/portfolio/src/pages/cv/Cv.vue
@@ -0,0 +1,12 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Cv',
+  props: {
+  },
+});
+</script>
+
+<template src="./Cv.html"></template>
+<style scoped lang="scss" src="./Cv.scss"></style>
