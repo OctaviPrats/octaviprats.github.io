0856dd69609104c5170aab794d579163b4429a5a
octaviprats <octavi.work@gmail.com>
2023-03-05 23:25:44 +0000

    continue

--- web-source/my-app/src/components/PageLayout.tsx
+++ web-source/my-app/src/components/PageLayout.tsx
@@ -1,13 +1,13 @@
-
+import '../src/styles/PageLayout.css'
 const PageLayout = (props: any) => {
-    return (
-        <div>
-            <a href="/">home</a>
-            <a href="/about-me">about me</a>
-            <a href="/projects">projects</a>
-            {props.children}
-        </div>
-    )
+  return (
+    <div className="main-page">
+      <a href="/">home</a>
+      <a href="/about-me">about me</a>
+      <a href="/projects">projects</a>
+      {props.children}
+    </div>
+  )
 }
 
-export default PageLayout;
+export default PageLayout
